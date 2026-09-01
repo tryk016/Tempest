c8009635828caa29450acaf485e035f4fb2ca736
Patrick Baran <patrick.baran@ewistore.co.uk>
2026-09-01 18:58:29 +0100

    Revert "Add portable temporal scaler API"
    This reverts commit 6c5f63e97d1164b78ad3015a7a20baf5ef62b43c.

--- Engine/graphics/encoder.h
+++ Engine/graphics/encoder.h
@@ -8,7 +8,6 @@
 #include <Tempest/UniformBuffer>
 #include <Tempest/AccelerationStructure>
 #include <Tempest/SpatialScaler>
-#include <Tempest/TemporalScaler>
 
 namespace Tempest {
 
@@ -122,8 +121,6 @@ class Encoder<Tempest::CommandBuffer> {
     void generateMipmaps(Attachment& tex);
 
     bool spatialUpscale(const SpatialScaler& scaler, const Attachment& input, StorageImage& output);
-    bool temporalUpscale(TemporalScaler& scaler, const Attachment& input, const ZBuffer& depth,
-                         const Attachment& motion, StorageImage& output, const TemporalScalerArgs& args);
 
   private:
     explicit Encoder(CommandBuffer* ow);
@@ -152,3 +149,4 @@ class Encoder<Tempest::CommandBuffer> {
   friend class CommandBuffer;
   };
 }
+
